2d71c71aa51bd97bbc3a6331b4b9d2e4e8fc39ea
Craig Neuwirt <cneuwirt@gmail.com>
2014-02-09 08:41:30 -0600

    Initial Commit

---
+++ Miruken/Miruken.h
@@ -0,0 +1,13 @@
+//
+//  Miruken.h
+//  Miruken
+//
+//  Created by Craig Neuwirt on 2/9/14.
+//  Copyright (c) 2014 Craig Neuwirt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Miruken : NSObject
+
+@end
---
+++ Miruken/Miruken.m
@@ -0,0 +1,13 @@
+//
+//  Miruken.m
+//  Miruken
+//
+//  Created by Craig Neuwirt on 2/9/14.
+//  Copyright (c) 2014 Craig Neuwirt. All rights reserved.
+//
+
+#import "Miruken.h"
+
+@implementation Miruken
+
+@end
